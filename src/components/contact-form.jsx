84988047138285d2182a737a0286d6fdef2c8c64
Karim Badwork <karim@badwork.fr>
2021-09-03 19:11:29 +0200

    test sendgrid 2

--- src/components/contact-form.jsx
+++ src/components/contact-form.jsx
@@ -15,21 +15,21 @@ const resetForm = () => {
     console.log('resetForm')
 }
 
-const emailNotification = (data) => {
-    console.log('data :')
-    console.log(data)
+// const emailNotification = (data) => {
+//     console.log('data :')
+//     console.log(data)
 
-    const sgMail = require('@sendgrid/mail');
-    sgMail.setApiKey(process.env.SENDGRID_API_KEY);
-    const msg = {
-        to: 'contact@badwork.fr',
-        from: 'karim@badwork.fr',
-        subject: 'Sending with SendGrid is Fun',
-        text: 'and easy to do anywhere, even with Node.js',
-        html: '<strong>and easy to do anywhere, even with Node.js</strong>',
-    };
-    sgMail.send(msg);
-}
+//     const sgMail = require('@sendgrid/mail');
+//     sgMail.setApiKey(process.env.SENDGRID_API_KEY);
+//     const msg = {
+//         to: 'contact@badwork.fr',
+//         from: 'karim@badwork.fr',
+//         subject: 'Sending with SendGrid is Fun',
+//         text: 'and easy to do anywhere, even with Node.js',
+//         html: '<strong>and easy to do anywhere, even with Node.js</strong>',
+//     };
+//     sgMail.send(msg);
+// }
 
 const validationSchema = yup.object().shape({
     name: yup.string()
@@ -117,5 +117,16 @@ const ContactForm = () => (
     </Formik>
   </div>
 )
+
+const sgMail = require('@sendgrid/mail');
+sgMail.setApiKey('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');
+const msg = {
+    to: 'contact@badwork.fr',
+    from: 'karim@badwork.fr',
+    subject: 'Sending with SendGrid is Fun',
+    text: 'and easy to do anywhere, even with Node.js',
+    html: '<strong>and easy to do anywhere, even with Node.js</strong>',
+};
+sgMail.send(msg);
 
 export default ContactForm
