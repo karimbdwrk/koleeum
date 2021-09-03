89ed1ff023dd01132c5de9969421e8e2f286792c
Karim Badwork <karim@badwork.fr>
2021-09-03 18:55:39 +0200

    test sendgrid

--- src/components/contact-form.jsx
+++ src/components/contact-form.jsx
@@ -15,6 +15,22 @@ const resetForm = () => {
     console.log('resetForm')
 }
 
+const emailNotification = (data) => {
+    console.log('data :')
+    console.log(data)
+
+    const sgMail = require('@sendgrid/mail');
+    sgMail.setApiKey(process.env.SENDGRID_API_KEY);
+    const msg = {
+        to: 'contact@badwork.fr',
+        from: 'karim@badwork.fr',
+        subject: 'Sending with SendGrid is Fun',
+        text: 'and easy to do anywhere, even with Node.js',
+        html: '<strong>and easy to do anywhere, even with Node.js</strong>',
+    };
+    sgMail.send(msg);
+}
+
 const validationSchema = yup.object().shape({
     name: yup.string()
         .min(2, 'Too Short!')
@@ -61,8 +77,9 @@ const ContactForm = () => (
             }),
         })
         .then(response => response.json())
+        .then(data => emailNotification(data))
         .then(data => console.log(data));
-
+        emailNotification()
         resetForm()
       }}
     >
